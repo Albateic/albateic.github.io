a718c3e1feee369b608fa1c90130eb7f9d472f34
kittcu <137061424+kittcu@users.noreply.github.com>
2024-01-18 13:13:51 +0000

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -1,1 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBM8PngCsA5yHbR6TKxQ_qZG7zhE9i4VUA",
+    authDomain: "kittcuchat.firebaseapp.com",
+    projectId: "kittcuchat",
+    storageBucket: "kittcuchat.appspot.com",
+    messagingSenderId: "668770835130",
+    appId: "1:668770835130:web:c08b459b70b7cc09508083"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
